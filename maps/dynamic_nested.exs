b6f542793d986374990ac9068787a158d95b9758
thebrianemory <the@brianemory.com>
2018-02-19 08:06:47 -0800

    Nested examples

---
+++ maps/dynamic_nested.exs
@@ -0,0 +1,6 @@
+defmodule Nested do
+  nested = %{
+    buttercup: %{actor: %{first: "Robin", last: "Wright"}, role: "princess"},
+    westley: %{actor: %{first: "Cary", last: "Ewles"}, role: "farm boy"}
+  }
+end
