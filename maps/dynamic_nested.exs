d58a32a2f4d87bd6e1a32b82cef2d5a20e139f7a
thebrianemory <the@brianemory.com>
2018-02-19 08:11:38 -0800

    More nested examples

--- maps/dynamic_nested.exs
+++ maps/dynamic_nested.exs
@@ -1,6 +1,4 @@
-defmodule Nested do
-  nested = %{
-    buttercup: %{actor: %{first: "Robin", last: "Wright"}, role: "princess"},
-    westley: %{actor: %{first: "Cary", last: "Ewles"}, role: "farm boy"}
-  }
-end
+nested = %{
+  buttercup: %{actor: %{first: "Robin", last: "Wright"}, role: "princess"},
+  westley: %{actor: %{first: "Cary", last: "Ewles"}, role: "farm boy"}
+}
